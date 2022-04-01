636813ace4d1bcea0c592e2df7c39167ba096e67
danilchugaev <dmchugaev@gmail.com>
2022-04-01 23:49:28 +0700

    add eslint and husky

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+	'env': {
+		'browser': true,
+		'es2021': true,
+	},
+	'extends': [
+		'eslint:recommended',
+		'plugin:vue/essential',
+		'plugin:@typescript-eslint/recommended',
+	],
+	'parser': 'vue-eslint-parser',
+	'parserOptions': {
+		'ecmaVersion': 'latest',
+		'parser': '@typescript-eslint/parser',
+		'sourceType': 'module',
+	},
+	'plugins': [
+		'vue',
+		'@typescript-eslint',
+	],
+	'globals': {
+		'module': true,
+		'defineProps': true,
+	},
+	'rules': {
+		'linebreak-style': [
+			'error',
+			'unix',
+		],
+		'quotes': [
+			'error',
+			'single',
+		],
+		'semi': [
+			'error',
+			'always',
+		],
+		'comma-dangle': [
+			'error',
+			'always',
+		],
+	},
+};
