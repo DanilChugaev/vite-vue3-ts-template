28ab393ee9b0db081678764bb2e0e110139a7b20
Danil Chugaev <dmchugaev@gmail.com>
2023-04-18 11:08:09 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path');
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+    resolve:{
+        alias:{
+            '~' : path.resolve(__dirname, './src')
+        },
+    },
+    plugins: [
+        vue(),
+    ],
+});
